ccd5723a29ea8aaa1492a91d372f32dd5c412bf0
m7Aei <mateuspm2015@hotmail.com>
2019-06-27 22:21:53 -0300

    Adicionando a parte de produtos no sistema

---
+++ APISupermecado/database/migrations/1561684534659_produtos_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProdutosSchema extends Schema {
+  up () {
+    this.create('produtos', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+      table.string('marca').notNullable()
+      table.string('nome').notNullable()
+      table.integer('preco').notNullable()
+      table.timestamp('dataValidade').notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('produtos')
+  }
+}
+
+module.exports = ProdutosSchema
